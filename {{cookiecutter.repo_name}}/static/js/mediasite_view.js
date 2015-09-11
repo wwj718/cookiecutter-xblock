f410f8772808dc1b15ba2e28d18a62c5ef7ddbb1
wwj <wuwenjie718@gmail.com>
2015-09-11 14:16:04 +0800

    rm mediasite

--- {{cookiecutter.repo_name}}/static/js/mediasite_view.js
+++
@@ -1,9 +0,0 @@
-/* Javascript for mediasiteXBlock. */
-function mediasiteXBlockInitView(runtime, element) {
-    /* Weird behaviour :
-     * In the LMS, element is the DOM container.
-     * In the CMS, element is the jQuery object associated*
-     * So here I make sure element is the jQuery object */
-     //get params from studio
-     console.log("mytest");
-}
